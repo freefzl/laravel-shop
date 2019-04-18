d1e75bda58429e41981b69270084e038b2958127
vagrant <vagrant@homestead>
2019-04-18 03:24:29 +0000

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -6,6 +6,11 @@
  * Time: 11:08
  */
 
-function test_helper() {
-    return 'OK';
+/**
+ * 此方法会将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制
+ * @return mixed
+ */
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
 }
