69f639efddb93384a5ed7d630c68fec0b5fd14e3
vagrant <vagrant@homestead>
2019-04-19 07:29:47 +0000

    完善支付后逻辑

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\OrderPaid;
 use App\Exceptions\InvalidRequestException;
 use App\Models\Order;
 use Carbon\Carbon;
@@ -57,6 +58,8 @@ class PaymentController extends Controller
         // 如果这笔订单的状态已经是已支付
         if ($order->paid_at) {
             // 返回数据给支付宝
+            $this->afterPaid($order);
+
             return app('alipay')->success();
         }
 
@@ -67,5 +70,10 @@ class PaymentController extends Controller
         ]);
 
         return app('alipay')->success();
+    }
+
+    protected function afterPaid(Order $order)
+    {
+        event(new OrderPaid($order));
     }
 }
